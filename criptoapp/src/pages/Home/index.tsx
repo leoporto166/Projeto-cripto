1456c1d444f8f93a757bc4c20c1d0b3d7724de85
leoporto166 <leonardo.porto166@gmail.com>
2025-06-20 19:48:13 -0300

    API

--- criptoapp/src/pages/Home/index.tsx
+++ criptoapp/src/pages/Home/index.tsx
@@ -1,16 +1,100 @@
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate  } from "react-router-dom"
 import home from "./home.module.css"
 import { BsSearch } from "react-icons/bs"
+import { useState, type FormEvent, useEffect } from "react"
+
+interface ApiProps{
+    id: string;
+    name: string;
+    symbol: string;
+    priceUsd: string;
+    vwap24Hr: string;
+    changePercent24Hr: string;
+    rank: string;
+    maxSupply:string;
+    supply: string
+    marketCapUsd: string
+    volumeUsd24Hr: string;
+    explorer: string;
+    precoFormatado?: string;
+    marketFormatado?: string;
+    volumeFormatado?: string;
+    mudancaFormatado24?: string;
+}
+
+interface DataProps{
+    data: ApiProps[]
+}
+
 
 export function Home(){
+    const [input, setInput] = useState("")
+    const [api, setApi] = useState<ApiProps[]>([])
+
+
+
+    const navigate = useNavigate()
+
+    function pesquisa(e: FormEvent){
+        e.preventDefault();
+
+        if(input === "") return;
+
+        navigate(`/detalhes/${input}`)
+    }
+
+    function handleGetMore(){
+        alert("CLICOU")
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    async function getData() {
+        fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=af8f8e664700c3f359e5119955b33df610b4f0f7584678aeb537c45ed63f8dee")
+        .then(response => response.json())
+        .then((data: DataProps)=> {
+            const apiData = data.data
+
+            const price = Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD"
+            })
+
+             const priceCompact = Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+                notation: "compact",
+            })
+
+            const resultadoFormatado = apiData.map((item) => {
+                const formatado = {
+                    ...item,
+                    precoFormatado: price.format(Number(item.priceUsd)),
+                    marketFormatado: priceCompact.format(Number(item.marketCapUsd)),
+                    volumeFormatado: priceCompact.format(Number(item.volumeUsd24Hr)),
+                    mudancaFormatado24: Number(item.changePercent24Hr).toFixed(2),
+                }
+                return formatado
+            })
+
+            console.log(resultadoFormatado )
+            setApi(resultadoFormatado);
+
+        })
+        
+    }
     return(
         
         <main className={home.container}>
-            <form className={home.form}>
+            <form className={home.form} onSubmit={pesquisa}>
                 <input
                 type="text"
                 placeholder="Nome da moeda"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
                 />
 
                 <button>
@@ -31,34 +115,56 @@ export function Home(){
                 </thead>
                 
                 <tbody id="tbody">
-                <tr className={home.tr}>
-                    <td className={home.td} data-label="Moeda">
+                
+                    {api.map((item) => (
+                     <tr className={home.tr} key={item.id}>
+
+                        
+
+                        <td className={home.td} data-label="Moeda">
                         <div>
-                            <Link to={"/detalhes/bitcoin"}>
-                            <span>Bitcoin</span> | BTC
+                            <img 
+                            className={home.img}
+                            src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}>
+                            </img>
+                            <Link to={`/detalhes/${item.id}`}>
+                            <span>{item.name}</span> | {item.symbol}
                             </Link>
                         </div>
                     </td>
 
                     <td className={home.td} data-label="Valor Mercado">
-                        1T
+                        {item.marketFormatado}
                     </td>
 
                     <td className={home.td} data-label="Preço">
-                        8.000
+                        {item.precoFormatado}
                     </td>
 
                     <td className={home.td} data-label="Volume">
-                        2B
+                        {item.volumeFormatado}
+                        
                     </td>
 
-                    <td className={home.tdProfit} data-label="Mudança 24h">
-                        <span>1.20</span>
+                    {Number(item.mudancaFormatado24) < 0 && (
+                         <td className={home.tdLoss} data-label="Mudança 24h">
+                        <span>{item.mudancaFormatado24}%</span>
                     </td>
+                    )}
+                    {Number(item.mudancaFormatado24) >= 0 && (
+                         <td className={home.tdProfit} data-label="Mudança 24h">
+                        <span>{item.mudancaFormatado24}%</span>
+                    </td>
+                    )}
 
-                </tr>
+                     </tr>
+                    ))}
+
+                
             </tbody>
             </table>
+
+            <button className={home.button} onClick={handleGetMore}>Mostrar Mais</button>
 
             
         </main>
